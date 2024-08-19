77f86ce3ae5d5778776cc5ca55a6de8aa7f7c3be
Adrian <X00194620@mytudublin.ie>
2024-08-19 15:04:30 +0100

    added threeJS

--- JsSrc/src/index.js
+++ JsSrc/src/index.js
@@ -1,10 +1,60 @@
-import _ from 'lodash';
+import * as THREE from 'three';
+import { ViewHelper } from 'three/addons/helpers/ViewHelper.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { AsciiEffect } from 'three/addons/effects/AsciiEffect.js';
 
-function component() {
-  const element = document.createElement('div');
+let width = window.innerWidth;
+let height =window.innerHeight;
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  return element;
+let camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 10000);
+camera.position.z = 30;
+
+scene.add(camera);
+
+let scene = new THREE.Scene();
+scene.background = new THREE.Color( 0, 0, 0 );
+
+const pointLight1 = new THREE.PointLight( 0xffffff, 3, 0, 0 );
+pointLight1.position.set( 500, 500, 500 );
+scene.add( pointLight1 );
+
+const pointLight2 = new THREE.PointLight( 0xffffff, 1, 0, 0 );
+pointLight2.position.set( - 500, - 500, - 500 );
+scene.add( pointLight2 );
+
+renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animate );
+effect = new AsciiEffect( renderer, ' .:-+*=%@#', { invert: true } );
+effect.setSize( window.innerWidth, window.innerHeight );
+effect.domElement.style.color = 'white';
+effect.domElement.style.backgroundColor = 'black';
+
+document.body.appendChild(effect.domElement);
+
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.maxDistance = 50;//~ Max zoom out distance ~//
+controls.update();
+resize();
+animate();
+
+window.addEventListener( 'resize', onWindowResize );
+function onWindowResize() {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  effect.setSize( window.innerWidth, window.innerHeight );
+
 }
-document.body.appendChild(component());
+
+
+
+function animate() {
+  controls.update();
+  effect.render(scene, camera);
+
+
+}
