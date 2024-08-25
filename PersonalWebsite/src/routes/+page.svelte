55a2f9cbbb5a82011faec8444980c27f1b300489
AdrianDonnelly <X00194620@mytudublin.ie>
2024-08-25 13:40:53 +0100

    fixes

--- PersonalWebsite/src/routes/+page.svelte
+++ PersonalWebsite/src/routes/+page.svelte
@@ -4,16 +4,13 @@
     let height;
   
     onMount(() => {
-      width = window.innerWidth;
-      height = window.innerHeight;
-  
-      // Your three.js setup code that depends on `window`
       // @ts-ignore
       import('../../static/JsSrc/dist/src.js').then((module) => {
         module.default(); // Call the default export if your JS module has one
       });
     });
     import { Separator } from "$lib/components/ui/separator";
+
   </script>
   
   <style>
@@ -21,21 +18,21 @@
   </style>
   
   <nav class="bg-black">
-    <div class="text-white m-2 ml-4">
+    <div class="text-white mt-4 ml-6 mr-6">
     <h1 class="scroll-m-20 text-2xl font-extrabold tracking-tight ">Adrian Donnelly</h1>
     <Separator class="my-4" />
 
-    <div class="flex flex-row gap-3">
-    <h1 class="scroll-m-20 text-l font-semibold tracking-tight"><a href="https://linkedin.com">LinkedIn</a></h1>
+    <div class="flex flex-row gap-3 text-muted-foreground ">
+    <h1 class="scroll-m-20 text-l font-semibold tracking-tight hover:text-white duration-200"><a href="https://www.linkedin.com/in/adrdonnelly/">LinkedIn</a></h1>
     <Separator orientation="vertical" />
 
-    <h1 class="scroll-m-20 text-l font-semibold tracking-tight"><a href="mailto:your.email@example.com">Email</a></h1>
+    <h1 class="scroll-m-20 text-l font-semibold tracking-tight hover:text-white duration-200"><a href="mailto:adrian.donnelly15@outlook.com">Email</a></h1>
     <Separator orientation="vertical" />
 
-    <h1 class="scroll-m-20 text-l font-semibold tracking-tight"><a href="https://github.com/your-profile">Github</a></h1>
+    <h1 class="scroll-m-20 text-l font-semibold tracking-tight hover:text-white duration-200"><a href="https://github.com/AdrianDonnelly">Github</a></h1>
     <Separator orientation="vertical" />
     
-    <h1 class="scroll-m-20 text-l font-semibold tracking-tight"><a href="https://your-portfolio.com">Portfolio</a></h1>
+    <h1 class="scroll-m-20 text-l font-semibold tracking-tight hover:text-white duration-200"><a href="https://your-portfolio.com">Portfolio</a></h1>
 </div>
 </div>
   </nav>
